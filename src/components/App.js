0cf65012bcba16009c6589f6485526ed8858e680
sufian <sufian4se@gmail.com>
2021-09-06 15:49:32 +0600

    lesson-4 source code push

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-
+import '../styles/App.css';
+import Layout from './Layout';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <>
+    <Layout>
+      <Home />
+    </Layout>
+    </>
   );
 }
 
